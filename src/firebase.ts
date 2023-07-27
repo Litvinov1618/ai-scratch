4e2f5c69b71db54d0e842d0bcb66b296ee1a2f34
Oleksandr Litvinov <litvinov1618@gmail.com>
2023-07-27 12:51:45 +0300

    Add .env.template

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "ai-scratch-613ce.firebaseapp.com",
-  projectId: "ai-scratch-613ce",
-  storageBucket: "ai-scratch-613ce.appspot.com",
-  messagingSenderId: "791340808475",
-  appId: "1:791340808475:web:856f59d424c62a35b6bbbe"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
